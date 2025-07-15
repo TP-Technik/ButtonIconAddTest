15c3835b96e385b1ca1b76366801de172690db58
Niklas <n.neuhold@teampool.engineering>
2025-07-15 14:26:02 +0200

    Add project files.

---
+++ ButtonIconAddTest/Marshal2.cs
@@ -0,0 +1,35 @@
+﻿using System.Runtime.InteropServices;
+
+public class Marshal2
+{
+
+    public static object? GetActiveObject(string progId, bool throwOnError = false)
+    {
+        if (progId == null)
+            throw new ArgumentNullException(nameof(progId));
+
+        var hr = CLSIDFromProgIDEx(progId, out var clsid);
+        if (hr < 0)
+        {
+            if (throwOnError)
+                Marshal.ThrowExceptionForHR(hr);
+
+            return null;
+        }
+
+        hr = GetActiveObject(clsid, nint.Zero, out var obj);
+        if (hr < 0)
+        {
+            if (throwOnError)
+                Marshal.ThrowExceptionForHR(hr);
+
+            return null;
+        }
+        return obj;
+    }
+    [DllImport("ole32")]
+    private static extern int CLSIDFromProgIDEx([MarshalAs(UnmanagedType.LPWStr)] string lpszProgID, out Guid lpclsid);
+
+    [DllImport("oleaut32")]
+    private static extern int GetActiveObject([MarshalAs(UnmanagedType.LPStruct)] Guid rclsid, nint pvReserved, [MarshalAs(UnmanagedType.IUnknown)] out object ppunk);
+}
---
+++ ButtonIconAddTest/Program.cs
@@ -0,0 +1,67 @@
+﻿using Inventor;
+using IPictureDisp = stdole.IPictureDisp;
+using Bitmap = System.Drawing.Bitmap;
+
+public class Programm
+{
+
+    private static String AddInClientID = "myAddInID";
+
+    private static RibbonTab? myCustomTab;
+    private static RibbonPanel? createPannel;
+    private static ButtonDefinition? myCustomButton;
+    private static CommandControl? myCustomButtonControl;
+
+    [STAThread]
+    public static void Main()
+    {
+        var invApp = Marshal2.GetActiveObject("Inventor.Application") as Application;
+
+        var ribbonRefference = invApp.UserInterfaceManager.Ribbons["Assembly"];
+
+        // Create custom Tab:
+        myCustomTab = ribbonRefference.RibbonTabs.Add(
+            "myCustomTab",
+            "InternalName1",
+            AddInClientID);
+
+        // Create custom Pannel:
+        createPannel = myCustomTab.RibbonPanels.Add(
+            "createPannel",
+            "InternalName2",
+            AddInClientID);
+
+        var controlDefs = invApp.CommandManager.ControlDefinitions;
+
+
+        Bitmap standardBmp = new Bitmap(@"C:\...\Plug16.bmp");
+        Bitmap largeBmp = new Bitmap(@"C:\...\Plug32.bmp");
+
+        IPictureDisp standardPict = PictureDispConverter.ToIPictureDisp(standardBmp);
+        IPictureDisp largePict = PictureDispConverter.ToIPictureDisp(largeBmp);
+
+
+        // Create custom Button:
+        myCustomButton = controlDefs.AddButtonDefinition(
+            "myCustomButton",
+            "InternalName3",
+            CommandTypesEnum.kEditMaskCmdType,
+            StandardIcon: standardPict,
+            LargeIcon: largePict);
+
+
+        myCustomButton.OnExecute += new ButtonDefinitionSink_OnExecuteEventHandler(myFunction);
+
+        // Add Button:
+        myCustomButtonControl = createPannel.CommandControls.AddButton(myCustomButton);
+
+    }
+
+    private static void myFunction(NameValueMap context)
+    {
+        // Do something
+    }
+
+}
+
+
